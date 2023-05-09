064fa1e07dcc2e860d95cbc69f5b450e29fc7619
Jh_Moshnikov <Jh.Moshnikov@yandex.ru>
2023-05-09 19:39:48 +0300

    11пр после ревью

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -12,10 +12,10 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
     useEffect(() => {
         setName(currentUser.name);
         setDescription(currentUser.about);
-    }, [currentUser]);
+    }, [currentUser, isOpen]);
 
     const handleChangeName = (e) => {
-        setName('');
+        setName(e.target.value);
     };
 
     const handleChangeDiscription = (e) => {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 function Footer() {
-  let year = new Date().getFullYear()
+  const year = new Date().getFullYear()
   return (
     <footer className="footer"><p className="footer__information">© {year} Mesto Russia</p></footer>
   )
